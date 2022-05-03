df82263d7d6b18e5be24d51c44e67a293f47aff8
PULKIT SONI <pulkit8290@gmail.com>
2022-05-03 22:46:20 +0530

    On branch master
    No commits yet
    
    Changes to be committed:
      (use "git rm --cached <file>..." to unstage)
    	new file:   RegForm/.classpath
    	new file:   RegForm/.gitignore
    	new file:   RegForm/.project
    	new file:   RegForm/.settings/.jsdtscope
    	new file:   RegForm/.settings/org.eclipse.jdt.core.prefs
    	new file:   RegForm/.settings/org.eclipse.wst.common.component
    	new file:   RegForm/.settings/org.eclipse.wst.common.project.facet.core.xml
    	new file:   RegForm/.settings/org.eclipse.wst.jsdt.ui.superType.container
    	new file:   RegForm/.settings/org.eclipse.wst.jsdt.ui.superType.name
    	new file:   RegForm/src/main/java/com/org/genp/day16/Register.java
    	new file:   RegForm/src/main/webapp/META-INF/MANIFEST.MF
    	new file:   RegForm/src/main/webapp/Register.html
    	new file:   RegForm/src/main/webapp/WEB-INF/lib/mysql-connector-java-8.0.29.jar
    	new file:   RegForm/src/main/webapp/WEB-INF/lib/servlet-api.jar
    	new file:   RegForm/src/main/webapp/WEB-INF/web.xml
    	new file:   SQLdatabase.sql

---
+++ SQLdatabase.sql
@@ -0,0 +1,8 @@
+create database userdb;
+use userdb;
+CREATE TABLE  REGISTERUSER   
+   (    NAME VARCHAR(40),   
+    PASS VARCHAR(40),   
+    EMAIL VARCHAR(40),   
+    COUNTRY VARCHAR(40)  
+   )  ;
